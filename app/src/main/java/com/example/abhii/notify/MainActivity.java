71d09486404cc076eae6443b6b6cc748b421c08b
roh-7 <rohit.6233@outlook.com>
2018-04-14 21:07:53 +0530

    firebase init

--- app/src/main/java/com/example/abhii/notify/MainActivity.java
+++ app/src/main/java/com/example/abhii/notify/MainActivity.java
@@ -1,41 +1,74 @@
 package com.example.abhii.notify;
 
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
-    RecyclerView rv;
-    FloatingActionButton fab;
-    List<Note> notes=new ArrayList<>();
-    NotesAdapter adapter;
+public class MainActivity extends AppCompatActivity
+{
+	RecyclerView rv;
+	FloatingActionButton fab;
+	List<Note> notes = new ArrayList<>();
+	NotesAdapter adapter;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        rv=findViewById(R.id.rv);
-        fab=findViewById(R.id.fab);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        init();
-        adapter = new NotesAdapter(this,notes);
-        rv.setAdapter(adapter);
-    }
-    public void init(){
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
-    notes.add(new Note("Happy Birthday" , "To you"));
+	FirebaseDatabase database = FirebaseDatabase.getInstance();
+	DatabaseReference ref;
 
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		rv = findViewById(R.id.rv);
+		fab = findViewById(R.id.fab);
+
+		ref = database.getReference("/notes");
+
+		ref.addListenerForSingleValueEvent(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot)
+			{
+				Log.v("TAG",dataSnapshot.toString());
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError)
+			{
+
+			}
+		});
+
+		rv.setLayoutManager(new LinearLayoutManager(this));
+		init();
+		adapter = new NotesAdapter(this, notes);
+		rv.setAdapter(adapter);
+
+	}
+
+
+	public void init()
+	{
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+		notes.add(new Note("Happy Birthday", "To you"));
+
+	}
 }
